799b44db7ffdd3454f154046655bd847266dc402
Emmanuel Godinez <manuel.godinez22@gmail.com>
2014-08-04 10:34:56 +1200

    Initial commit. Image Enhancer done.

---
+++ ImageEnhancement/src/main/ImageEnhancement.java
@@ -0,0 +1,38 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+public class ImageEnhancement {
+	public static void main(String[] args){
+		if(args.length!=1){
+			JOptionPane.showMessageDialog(null, "One argument please");
+			return;
+		}
+		if(!args[0].endsWith(".png")){
+			JOptionPane.showMessageDialog(null, "PNG file please");
+			return;
+		}
+		
+		new ImageEnhancement(args[0]);
+	}
+	
+	public ImageEnhancement(String imagepath){
+		int[][] input = ImageHandler.readImage(imagepath);
+		ImageHandler.displayImage("original", input);
+		
+		// create enhancement filter
+		double[][] laplacianMask1 = { { 0, -1, 0 }, { -1, 5, -1 }, { 0, -1, 0 } };
+		double[][] laplacianMask2 = { { 1, -2, 1 }, { -2, 5, -2 }, { 1, -2, 1 } };
+		
+		// apply enhancement filter
+		int[][] output = ImageHandler.applyConvolutionMask(input, laplacianMask1);
+		output = ImageHandler.brightenImage(output);
+		try {
+			Thread.sleep(30);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ImageHandler.displayImage("new image", output);
+	}
+}
---
+++ ImageEnhancement/src/main/mypanel.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class mypanel extends JPanel{
+
+	private int[][] image;
+
+	@Override
+	public void paint(Graphics g){
+		super.paint(g);
+		g.setColor(Color.white);
+		g.fillRect(0, 0, 1000, 1000);
+
+		for (int y = 0; y < image[0].length; y++) {
+			for (int x = 0; x < image.length; x++) {
+				int co = image[x][y]/3;
+				g.setColor(new Color(co,co,co));
+				g.drawRect(20+x, 20+y, 1, 1);
+			}
+		}
+	}
+
+	public mypanel(int[][] thing){
+		this.image = thing;
+		for (int i = 0; i < 10; i++) {
+			repaint();
+		}
+		
+		
+	}
+
+}
